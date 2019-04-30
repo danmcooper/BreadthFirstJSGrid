4217e385f354628be69dcab6e0d768f3b73a87b9
Dan <pdxdmc@gmail.com>
2019-04-29 20:03:10 -0700

    refactored

--- grid-path-problem.js
+++ grid-path-problem.js
@@ -1,16 +1,8 @@
 const GRID = {
-  VALUES: {
-    END: 9,
-    BLOCKED: 0,
-    EMPTY: 1
-  },
-  STATUS: {
-    VALID: 1000,
-    INVALID: 1001
-  },
-  INTERNAL: {
-    VISITED: 1003
-  }
+  END: "END",
+  BLOCKED: "BLOCKED",
+  EMPTY: "EMPTY",
+  VISITED: "VISITED"
 };
 
 const DIR = {
@@ -25,9 +17,13 @@ class Location {
     this.distanceFromTop = distanceFromTop;
     this.distanceFromLeft = distanceFromLeft;
     this.path = path;
-    this.initialStatus = this.status();
   }
-  status() {
+
+  visit() {
+    Location.grid[this.distanceFromTop][this.distanceFromLeft] = GRID.VISITED;
+  }
+
+  isInRange() {
     const gridHeight = Location.grid.length;
     const gridLength = Location.grid[0].length;
 
@@ -37,22 +33,21 @@ class Location {
       this.distanceFromTop < 0 ||
       this.distanceFromTop >= gridHeight
     ) {
-      // location is not on the grid--return false
-      return GRID.STATUS.INVALID;
-    } else if (
-      Location.grid[this.distanceFromTop][this.distanceFromLeft] ===
-      GRID.VALUES.END
-    ) {
-      return GRID.VALUES.END;
-    } else if (
-      Location.grid[this.distanceFromTop][this.distanceFromLeft] !==
-      GRID.VALUES.EMPTY
-    ) {
-      // location is either an obstacle or has been visited
-      return GRID.VALUES.BLOCKED;
-    } else {
-      return GRID.STATUS.VALID;
+      return false;
     }
+    return true;
+  }
+
+  isEnd() {
+    return (
+      Location.grid[this.distanceFromTop][this.distanceFromLeft] === GRID.END
+    );
+  }
+
+  isEmpty() {
+    return (
+      Location.grid[this.distanceFromTop][this.distanceFromLeft] === GRID.EMPTY
+    );
   }
 }
 
@@ -64,13 +59,17 @@ const findShortestPath = (startCoordinates, grid) => {
 
   while (queue.length > 0) {
     const currentLocation = queue.shift();
+    currentLocation.visit();
 
     for (let key in DIR) {
-      var newLocation = goDirection(currentLocation, DIR[key], grid);
-      if (newLocation.status() === GRID.VALUES.END) {
-        return newLocation.path;
-      } else if (newLocation.initialStatus === GRID.STATUS.VALID) {
-        queue.push(newLocation);
+      const newLocation = goDirection(currentLocation, DIR[key], grid);
+      if (newLocation.isInRange()) {
+        if (newLocation.isEnd()) {
+          return newLocation.path;
+        }
+        if (newLocation.isEmpty()) {
+          queue.push(newLocation);
+        }
       }
     }
   }
@@ -94,35 +93,28 @@ const goDirection = (currentLocation, direction) => {
     newDfl -= 1;
   }
 
-  const newLocation = new Location(newDft, newDfl, newPath);
-
-  if (newLocation.status() === GRID.STATUS.VALID) {
-    Location.grid[newLocation.distanceFromTop][newLocation.distanceFromLeft] =
-      GRID.INTERNAL.VISITED;
-  }
-
-  return newLocation;
+  return new Location(newDft, newDfl, newPath);
 };
 
 // prettier-ignore
 let testCases = [
   [
-    [GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY],
-    [GRID.VALUES.EMPTY,GRID.VALUES.BLOCKED,GRID.VALUES.BLOCKED,GRID.VALUES.BLOCKED],
-    [GRID.VALUES.EMPTY,GRID.VALUES.BLOCKED,GRID.VALUES.END,GRID.VALUES.EMPTY],
-    [GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY]
+    [GRID.EMPTY,GRID.EMPTY,GRID.EMPTY,GRID.EMPTY],
+    [GRID.EMPTY,GRID.BLOCKED,GRID.BLOCKED,GRID.BLOCKED],
+    [GRID.EMPTY,GRID.BLOCKED,GRID.END,GRID.EMPTY],
+    [GRID.EMPTY,GRID.EMPTY,GRID.EMPTY,GRID.EMPTY]
   ],
   [
-    [GRID.VALUES.EMPTY,GRID.VALUES.BLOCKED,GRID.VALUES.BLOCKED],
-    [GRID.VALUES.EMPTY,GRID.VALUES.BLOCKED,GRID.VALUES.BLOCKED],
-    [GRID.VALUES.EMPTY,GRID.VALUES.END,GRID.VALUES.EMPTY]
+    [GRID.EMPTY,GRID.BLOCKED,GRID.BLOCKED],
+    [GRID.EMPTY,GRID.BLOCKED,GRID.BLOCKED],
+    [GRID.EMPTY,GRID.END,GRID.EMPTY]
   ],
   [
-    [GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY],
-    [GRID.VALUES.BLOCKED,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY],
-    [GRID.VALUES.BLOCKED,GRID.VALUES.EMPTY,GRID.VALUES.BLOCKED,GRID.VALUES.EMPTY],
-    [GRID.VALUES.EMPTY,GRID.VALUES.EMPTY,GRID.VALUES.END,GRID.VALUES.EMPTY],
-    [GRID.VALUES.BLOCKED,GRID.VALUES.BLOCKED,GRID.VALUES.EMPTY,GRID.VALUES.EMPTY]
+    [GRID.EMPTY,GRID.EMPTY,GRID.EMPTY,GRID.EMPTY],
+    [GRID.BLOCKED,GRID.EMPTY,GRID.EMPTY,GRID.EMPTY],
+    [GRID.BLOCKED,GRID.EMPTY,GRID.BLOCKED,GRID.EMPTY],
+    [GRID.EMPTY,GRID.EMPTY,GRID.END,GRID.EMPTY],
+    [GRID.BLOCKED,GRID.BLOCKED,GRID.EMPTY,GRID.EMPTY]
   ]
 ];
 
